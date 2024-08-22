d827e3a8f1ff150c11ba98d60a217a9bdd2c82aa
xadicha <mahkamovahadija1810@gmail.com>
2024-08-22 20:26:39 +0500

    last

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"database/sql"
 	"net/http"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	"github.com/minio/minio-go"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	_ "lessons/ci_cd/docs"
 )
 
-type UrlRes struct {
-	MinioUrl string `json:"minio_url"`
-	URL      string `json:"url"`
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "postres://postgres:1@postgres-db:5432/postgres?sslmode=disable")
+	return db, err
 }
 
 type PingResponse struct {
 	Message string `json:"message"`
+}
+
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 func PingHandler(c *gin.Context) {
@@ -32,80 +32,33 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", PingHandler)
-	r.POST("/media", UploadMedia)
+	r.POST("/users", Create)
 
-	url := ginSwagger.URL("swagger/doc.json")
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
-
-	r.Run(":50051")
-
+	r.Run("test_cicd:50051")
 }
 
-// UploadMedia
-// @Summary     Upload Photo
-// @Security    BearerAuth
-// @Description Through this api front-ent can upload photo and get the link to the photo.
-// @Tags        MEDIA
-// @Accept      json
-// @Produce     json
-// @Param       file formData file true "Image"
-// @Success     200 {object} UrlRes
-// @Failure     400 {object} string
-// @Failure     500 {object} string
-// @Router      /media [POST]
-func UploadMedia(c *gin.Context) {
-
-	mFile, err := c.FormFile("file")
+func Create(c *gin.Context) {
+	db, err := initDB()
 	if err != nil {
-		c.AbortWithStatusJSON(400, gin.H{
-			"error": err.Error(),
-		})
 		return
 	}
 
-	ext := mFile.Filename
+	defer db.Close()
 
-	println("\n", ext)
-
-	minioClient, err := minio.New("3.79.185.212:9000", "minioadmin", "minioadmin", false)
+	var userRes User
+	err = c.ShouldBindJSON(&userRes)
 	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": err.Error(),
-		})
+		println(err)
+		return
 	}
 
-	err = minioClient.MakeBucket("photos", "")
+	query := `INSERT INTO user (id, name, email) VALUES ($1, $2, $3)`
+
+	_, err = db.Exec(query, userRes.ID, userRes.Name, userRes.Email)
 	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": err.Error(),
-		})
+		return
 	}
 
-	uploadDir := "/media"
-
-	url := filepath.Join(uploadDir, mFile.Filename)
-
-	_, err = minioClient.FPutObjectWithContext(context.Background(), "photos", mFile.Filename, uploadDir, minio.PutObjectOptions{
-		ContentType: "image/jpeg",
-	})
-	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": err.Error(),
-		})
-	}
-
-	err = c.SaveUploadedFile(mFile, url)
-	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": err.Error(),
-		})
-	}
-
-	minioUrl := fmt.Sprintf("http://3.79.185.212:9000/photos/%s", mFile.Filename)
-
-	c.JSON(200, &UrlRes{
-		MinioUrl: minioUrl,
-		URL:      url,
-	})
+	println("User name: ", userRes.Name, "\n Email: ", userRes.Email)
 
 }
